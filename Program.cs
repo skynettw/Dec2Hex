7afc7b160a06cbeb52a259de2a0d8ae2598c4236
Richard <skynet.tw@gmail.com>
2022-03-22 19:37:14 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec2Hex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string symbols = "0123456789ABCDEF";
+            string hex_number = "";
+            Console.Write("請輸入您要轉換的10進位數字：");
+            number = Convert.ToInt32(Console.ReadLine());  
+            while (number > 1)
+            {
+                hex_number = (symbols[number % 16]).ToString() + hex_number;
+                number /=16 ;
+            }
+            Console.WriteLine("Hex numbers is {0}", hex_number);
+            Console.ReadLine();
+        }
+    }
+}
